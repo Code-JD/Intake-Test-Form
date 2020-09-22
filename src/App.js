e577b404bf8e7ed57c3299bb1fe9cdb34482d9eb
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-09-22 16:05:22 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik } from "formik";
+import { TextField } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+	return <div>
+		<Formik initialValues={{ firstName: 'bob' }} >
+			{ () => (
+			<form>
+				<TextField />
+			</form>
+		)}</Formik>
+	</div>;
+};
+
 
 export default App;
